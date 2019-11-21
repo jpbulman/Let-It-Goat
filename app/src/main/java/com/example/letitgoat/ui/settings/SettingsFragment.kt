c3c22390c5d3cf86c0b2450d034af5716eb03cdb
JP Bulman <jpbulman@wpi.edu>
2019-11-21 15:40:45 -0500

    Logout button added

--- app/src/main/java/com/example/letitgoat/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/letitgoat/ui/settings/SettingsFragment.kt
@@ -1,12 +1,16 @@
 package com.example.letitgoat.ui.settings
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
+import android.widget.Button
 import android.widget.SearchView
 import android.widget.TextView
+import androidx.core.view.marginBottom
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.letitgoat.MainActivity
 import com.example.letitgoat.R
 
 class SettingsFragment : Fragment() {
@@ -23,12 +27,16 @@ class SettingsFragment : Fragment() {
         settingsViewModel =
             ViewModelProviders.of(this).get(SettingsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
-        val textView: TextView = root.findViewById(R.id.text_settings)
-        settingsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
 
         setHasOptionsMenu(true)
+
+        val logoutButton = root.findViewById<Button>(R.id.logoutButton)
+
+        logoutButton.setOnClickListener{
+            MainActivity.user = null
+            startActivity(Intent(activity, MainActivity::class.java))
+        }
+
         return root
     }
 
