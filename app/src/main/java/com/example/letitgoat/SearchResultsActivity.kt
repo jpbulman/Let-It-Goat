9f096afdf909889e1f7786202479963b28f8aade
Matthew Kaminski <kaminskimateusz7@gmail.com>
2019-12-05 15:17:50 -0500

    Added Search activity/fragment

--- app/src/main/java/com/example/letitgoat/SearchResultsActivity.kt
+++ app/src/main/java/com/example/letitgoat/SearchResultsActivity.kt
@@ -2,25 +2,41 @@ package com.example.letitgoat
 
 import android.app.Activity
 import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class SearchResultsActivity : Activity() {
+class SearchResultsActivity : AppCompatActivity() {
+    var searchQuery = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            searchQuery = query
+        }
+
+        setContentView(R.layout.activity_search)
         handleIntent(intent)
     }
 
-    override fun onNewIntent(intent: Intent) {
-        handleIntent(intent)
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_search, menu)
+        // Associate searchable configuration with the SearchView
+        return true
     }
 
     private fun handleIntent(intent: Intent) {
-        println("1234567890")
-        if (Intent.ACTION_SEARCH == intent.action) {
-            val query = intent.getStringExtra(SearchManager.QUERY)
-            Toast.makeText(applicationContext, query, Toast.LENGTH_LONG).show()
-        }
     }
 }
